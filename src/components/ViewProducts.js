1b1a97827a15a6cb6f11cbdaf41e962a0fef5fbb
anonturtle <rajonali44492@gmail.com>
2020-10-19 00:19:19 -0500

    Added card to view product comp

--- src/components/ViewProducts.js
+++ src/components/ViewProducts.js
@@ -1,7 +1,7 @@
 import React, {Component, useEffect, useState} from 'react';
 import {db} from '../firebase/firebase';
 import ProductListItem from './ProductListItem';
-import {Table, Container} from 'react-bootstrap';
+import {Table,Card, Container} from 'react-bootstrap';
 
 function ViewProducts() {
 
@@ -24,7 +24,9 @@ function ViewProducts() {
 
 
             return (
-                <Container >
+                <Container>
+                                    <Card style={{padding:15}}>
+
                     <br />
                     <h1>View Products</h1>
 <Table>
@@ -45,6 +47,8 @@ function ViewProducts() {
                 </tbody>
 
                 </Table>
+                </Card>
+
                 </Container>
             );
     }
